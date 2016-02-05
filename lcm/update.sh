8553e1ec65eb273c9018aae1a34534a9bd8a6cbe
John R. Hoare <john.hoare@kitware.com>
2016-02-05 15:07:42 -0500

    Added tracking scripts to track upstream project.

---
+++ lcm/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="lcm"
+readonly ownership="LCM Upstream <kwrobot@kitware.com>"
+readonly subtree="$name/$name-src"
+readonly repo="https://github.com/lcm-proj/lcm.git"
+readonly tag="v1.0.0"
+
+extract_source () {
+    git_archive
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
